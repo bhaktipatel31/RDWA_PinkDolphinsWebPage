8efcdb709561146d1d2f75d924b1cd47d5f8718d
sms761 <90295322+sms761@users.noreply.github.com>
2021-11-10 10:16:44 -0500

    Update style.js

--- style.js
+++ style.js
@@ -2,7 +2,7 @@ var oldPicture;
 var oldTarget = 0;
 function restorePicture() {
     if (oldTarget == 1) {
-        document.getElementById("Sejal1").src = "https://i.pinimg.com/236x/02/6a/cc/026acca08fb7beea6bd4ecd430e312bd.jpg";
+        document.getElementById("Sejal1").src = "https://media4.giphy.com/media/IMfi5ugpf3W1y/giphy.gif";
     }
     if (oldTarget == 2) {
         document.getElementById("Bhakti1").src = "https://i.pinimg.com/564x/40/e6/29/40e629ddb31aedacdf2c5e94cfc7e23a.jpg";
@@ -17,9 +17,8 @@ function restorePicture() {
         document.getElementById("Niveditha1").src = "https://global.canon/ja/c-museum/wp-content/uploads/2021/02/dslr892_top.jpg";
     }
     if (oldTarget == 6) {
-        document.getElementById("Jovita1").src = "https://scontent.fewr1-6.fna.fbcdn.net/v/t1.18169-9/11701079_1108700615814049_3893307625563998154_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=kKp3VeG5OLMAX_0UWeU&_nc_ht=scontent.fewr1-6.fna&oh=a52b7e1e4f3ace58a648fee139b0107d&oe=61B1CFCE";
+        document.getElementById("Jovita1").src = "https://cdn2.vectorstock.com/i/1000x1000/52/31/cute-woman-avatar-casual-vector-12875231.jpg";
     }
-
 }
 function toggleVis(id) {
     var x = document.getElementById(id);
@@ -31,13 +30,15 @@ function toggleVis(id) {
 }
 
 function changeSejal(event) {
-    event.target.src = "https://i.pinimg.com/originals/15/a4/63/15a463647cf3353b62b372ba1551c337.png";
+    event.target.src = "https://media2.giphy.com/media/auvPdJqCpMA4E/200.gif";
     restorePicture();
     oldTarget = 1;
     document.getElementById("sejaldescription").innerHTML = "Sejal has 2 dogs named <strong>Coco and Sweety</strong>!";
 }
 
 document.getElementById("Sejal1").addEventListener("click", changeSejal);
+
+//------------------------------------------------------------------------------------------------------------------------------
 
 function changeBhakti(event) {
     event.target.src = "https://onmyojiguide.com/wp-content/uploads/2017/01/Glowing-Grass-Onmyoji-Shikigami-Icon-2-1.png";
@@ -48,6 +49,8 @@ function changeBhakti(event) {
 
 document.getElementById("Bhakti1").addEventListener("click", changeBhakti);
 
+//------------------------------------------------------------------------------------------------------------------------------
+
 function changeSruthi(event) {
     event.target.src = "https://upload.wikimedia.org/wikipedia/en/5/53/Snoopy_Peanuts.png";
     restorePicture();
@@ -57,34 +60,43 @@ function changeSruthi(event) {
 
 document.getElementById("Sruthi1").addEventListener("click", changeSruthi);
 
+//------------------------------------------------------------------------------------------------------------------------------
 
 function changeUnnati(event) {
-    event.target.src = "https://previews.123rf.com/images/diplograma/diplograma2008/diplograma200800567/153696749-cute-boba-bubble-milk-tea-with-tapioca-pearl-milk-tea-black-delicious-pearls-is-taiwanese-famous-pop.jpg";
+    event.target.src = "https://media.dayoftheshirt.com/images/shirts/9nDCm/teepublic_cute-kawaii-bubble-tea-boba-milk-cat-lover-gift-idea-teepublic_1576507293.large.png";//put your second picture here
     restorePicture();
-    oldtarget = 4;
-    document.getElementById("unnatidescription").innerHTML = "Unnati <strong>loves</strong> strawberry mochi :P"
+    oldTarget = 4;
+    document.getElementById("unnatidescription").innerHTML = "Unnati loves <strong>bubble tea</strong>!";
 }
+
 document.getElementById("Unnati1").addEventListener("click", changeUnnati);
+
+//------------------------------------------------------------------------------------------------------------------------------
 
 function changeNiveditha(event) {
     event.target.src = "https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/dslr-camera/desktop/DSLR_P1_900x420.jpg.img.jpg";
     restorePicture();
-    oldtarget = 5;
-    document.getElementById("Nivedithadescription").innerHTML = "Nive <strong>loves</strong> photography <3"
+    oldTarget = 5;
+    document.getElementById("Nivedithadescription").innerHTML = "Nive <strong>loves</strong> photography <3";
 }
+
 document.getElementById("Niveditha1").addEventListener("click", changeNiveditha);
 
+//------------------------------------------------------------------------------------------------------------------------------
+
 function changeJovita(event) {
-    event.target.src = "https://i.pinimg.com/564x/6c/9e/8c/6c9e8c46a9dfbd851afd92446ec5220a.jpg";
+    event.target.src = "https://media.istockphoto.com/vectors/frozen-yogurt-graphic-vector-id483509751?k=20&m=483509751&s=612x612&w=0&h=KaPRg4Xhoe-AZrk12ui5__bN1Ng33w2eqXjL8ifzzSo=";
     restorePicture();
-    oldtarget = 6;
-    document.getElementById("jovitadescription").innerHTML = "Jovita <strong>loves</strong> froyo :D"
+    oldTarget = 6;
+    document.getElementById("jovitadescription").innerHTML = "Jovita loves <strong>frozen yogurt</strong>!"
 }
+
 document.getElementById("Jovita1").addEventListener("click", changeJovita);
 
+//------------------------------------------------------------------------------------------------------------------------------
 
 function reset() {
-    document.getElementById("Sejal1").src = "https://i.pinimg.com/236x/02/6a/cc/026acca08fb7beea6bd4ecd430e312bd.jpg";
+    document.getElementById("Sejal1").src = "https://media4.giphy.com/media/IMfi5ugpf3W1y/giphy.gif";
     document.getElementById("sejaldescription").innerHTML = "";
 
     document.getElementById("Bhakti1").src = "https://i.pinimg.com/564x/40/e6/29/40e629ddb31aedacdf2c5e94cfc7e23a.jpg";
@@ -99,7 +111,7 @@ function reset() {
     document.getElementById("Niveditha1").src = "https://global.canon/ja/c-museum/wp-content/uploads/2021/02/dslr892_top.jpg";
     document.getElementById("Nivedithadescription").innerHTML = "";
 
-    document.getElementById("Jovita1").src = "https://scontent.fewr1-6.fna.fbcdn.net/v/t1.18169-9/11701079_1108700615814049_3893307625563998154_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=kKp3VeG5OLMAX_0UWeU&_nc_ht=scontent.fewr1-6.fna&oh=a52b7e1e4f3ace58a648fee139b0107d&oe=61B1CFCE";
+    document.getElementById("Jovita1").src = "https://cdn2.vectorstock.com/i/1000x1000/52/31/cute-woman-avatar-casual-vector-12875231.jpg";
     document.getElementById("jovitadescription").innerHTML = "";
 
 }
