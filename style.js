dd014ed85f783d060004d984b74dc51801bccb88
Bhakti Patel <62191831+bhaktipatel31@users.noreply.github.com>
2021-11-09 19:58:54 -0500

    Update style.js

--- style.js
+++ style.js
@@ -1,20 +1,80 @@
-var oldpic; //null for now 
-var oldtarget = 0; //null for now 
+var oldPicture;
+var oldTarget = 0;
 function restorePicture() {
-    if (oldtarget = 1) {
+    if (oldTarget == 1) {
         document.getElementById("Sejal1").src = "https://i.pinimg.com/236x/02/6a/cc/026acca08fb7beea6bd4ecd430e312bd.jpg";
     }
-    if (oldtarget = 2) {
+    if (oldTarget == 2) {
         document.getElementById("Bhakti1").src = "https://i.pinimg.com/564x/40/e6/29/40e629ddb31aedacdf2c5e94cfc7e23a.jpg";
     }
+    if (oldTarget == 3) {
+        document.getElementById("Sruthi1").src = "https://www.nicepng.com/png/full/161-1615668_super-mario-sunshine-princess-peach.png";
+    }
+}
+function toggleVis(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function changeSejal(event) {
+    event.target.src = "https://i.pinimg.com/originals/15/a4/63/15a463647cf3353b62b372ba1551c337.png";
+    restorePicture();
+    oldTarget = 1;
+    document.getElementById("sejaldescription").innerHTML = "Sejal has 2 dogs named <strong>Coco and Sweety</strong>!";
+}
+
+document.getElementById("Sejal1").addEventListener("click", changeSejal);
+
+function changeBhakti(event) {
+    event.target.src = "https://onmyojiguide.com/wp-content/uploads/2017/01/Glowing-Grass-Onmyoji-Shikigami-Icon-2-1.png";
+    restorePicture();
+    oldTarget = 2;
+    document.getElementById("bhaktidescription").innerHTML = "Bhakti <strong>loves</strong> Brown Sugar Bubble tea ^_^";
+}
+
+document.getElementById("Bhakti1").addEventListener("click", changeBhakti);
+
+function changeSruthi(event) {
+    event.target.src = "https://upload.wikimedia.org/wikipedia/en/5/53/Snoopy_Peanuts.png";
+    restorePicture();
+    oldTarget = 3;
+    document.getElementById("sruthidescription").innerHTML = "Sruthi <strong>loves</strong> her dog Jupiter <3";
+}
+
+document.getElementById("Sruthi1").addEventListener("click", changeSruthi);
+
+//add
+
+
+function reset() {
+    document.getElementById("Sejal1").src = "https://i.pinimg.com/236x/02/6a/cc/026acca08fb7beea6bd4ecd430e312bd.jpg";
+    document.getElementById("sejaldescription").innerHTML = "";
+
+    document.getElementById("Bhakti1").src = "https://i.pinimg.com/564x/40/e6/29/40e629ddb31aedacdf2c5e94cfc7e23a.jpg";
+    document.getElementById("bhaktidescription").innerHTML = "";
+
+    document.getElementById("Sruthi1").src = "https://upload.wikimedia.org/wikipedia/en/5/53/Snoopy_Peanuts.png";
+    document.getElementById("sruthidescription").innerHTML = "";
+
+//add
+}
+document.getElementById("reset").addEventListener("click", reset);
+
+
+//above is updated
+
+
+
+
     if (oldtarget = 3) {
         document.getElementById("Unnati1").src"http://vivianwskwan.com/wp-content/uploads/2018/06/mochimii_family-gif-500.gif";
     }
     if (oldtarget = 4) {
         document.getElementById("Jovita1").src"";
-    }
-    if (oldtarget = 5) {
-        document.getElementById("Sruthi1").src"";
     }
     if (oldtarget = 6) {
         document.getElementById("Niveditha1").src"";
@@ -22,25 +82,7 @@ function restorePicture() {
 }
 
 
-function changeSejal(event) {
-    console.dir(oldtarget);
-    event.target.src = "https://i.pinimg.com/originals/15/a4/63/15a463647cf3353b62b372ba1551c337.png";
-    restorePicture();
-    oldtarget = 1;
-    document.getElementById("description").innerHTML = "Hello!";
-}
-document.getElementById("Sejal1").addEventListener("click", changeSejal)
-//Sejal
 
-function changeBhakti(event) {
-    console.dir(oldtarget);
-    event.target.src = "https://onmyojiguide.com/wp-content/uploads/2017/01/Glowing-Grass-Onmyoji-Shikigami-Icon-2-1.png";
-    restorePicture();
-    oldtarget = 2;
-    document.getElementById("description").innerHTML = "Bhakti <strong>loves<strong> Brown Sugar Bubble tea ^_^";
-}
-document.getElementById("Bhakti1").addEventListener("click", changeBhakti);
-//Bhakti 
 
 function changeUnnati(event) {
     console.dir(oldtarget);
@@ -68,18 +110,7 @@ function changeJovita(event) {
 document.getElementById("Jovita1").addEventListener("click", changeJovita);
 //Jovita 
 
-function changeSruthi(event) {
-    console.dir(oldtarget);
-    event.target.src = "https://www.nicepng.com/maxp/u2w7a9q8e6w7q8q8/";
-    if (oldtarget !== null) {
-        oldtarget.src = oldpic;
-    }
-    oldtarget = event.target;
-    oldpic = "https://static.wikia.nocookie.net/id5/images/9/92/AdaMesmer.png/revision/latest/scale-to-width-down/371?cb=20210719132900";
-    document.getElementById("description").innerHTML = "Sruthi <strong>loves<strong> her dog Jupiter <3"
-}
-document.getElementById("Sruthi1").addEventListener("click", changeSruthi);
-//Sruthi 
+
 
 function changeNiveditha(event) {
     console.dir(oldtarget);
